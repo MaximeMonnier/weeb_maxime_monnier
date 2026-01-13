ed343c17b339aba4b8eae7efc1c2c3d5b7a3e863
Maxime Monnier <maximemonnier39@gmail.com>
2026-01-13 22:03:47 +0100

    feat: Add the folder structure for the app and creat fild for the home page with components

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/common/NavBar";
+
+const home = () => {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default home;
